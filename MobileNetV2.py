c905cd020095cd832d3e6913b3fb8c994896efe8
ahirner <a.hirner@gmail.com>
2018-01-27 16:35:35 +0100

    Adhere to torchvision kwarg, separate classifier from backbone

--- MobileNetV2.py
+++ MobileNetV2.py
@@ -49,9 +49,8 @@ class InvertedResidual(nn.Module):
             return self.conv(x)
 
 
-class MobileNetV2(nn.Module):
-    def __init__(self, n_class=1000, input_size=224, width_mult=1.):
-        super(MobileNetV2, self).__init__()
+class MobileNetV2Backbone(nn.Sequential):
+    def __init__(self, input_size=224, width_mult=1.):
         # setting of inverted residual blocks
         self.interverted_residual_setting = [
             # t, c, n, s
@@ -68,33 +67,57 @@ class MobileNetV2(nn.Module):
         assert input_size % 32 == 0
         input_channel = int(32 * width_mult)
         self.last_channel = int(1280 * width_mult) if width_mult > 1.0 else 1280
-        self.features = [conv_bn(3, input_channel, 2)]
+        features = [conv_bn(3, input_channel, 2)]
+
         # building inverted residual blocks
         for t, c, n, s in self.interverted_residual_setting:
             output_channel = int(c * width_mult)
             for i in range(n):
                 if i == 0:
-                    self.features.append(InvertedResidual(input_channel, output_channel, s, t))
+                    features.append(InvertedResidual(input_channel,
+                                                     output_channel, s, t))
                 else:
-                    self.features.append(InvertedResidual(input_channel, output_channel, 1, t))
+                    features.append(InvertedResidual(input_channel,
+                                                     output_channel, 1, t))
                 input_channel = output_channel
+
         # building last several layers
-        self.features.append(conv_1x1_bn(input_channel, self.last_channel))
-        self.features.append(nn.AvgPool2d(input_size//32))
-        # make it nn.Sequential
-        self.features = nn.Sequential(*self.features)
+        features.append(conv_1x1_bn(input_channel, self.last_channel))
+        features.append(nn.AvgPool2d(input_size//32))
+
+        super(MobileNetV2Backbone, self).__init__(*features)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000, input_size=224, width_mult=1.):
+        super(MobileNetV2, self).__init__()
+        # setting of inverted residual blocks
+        self.interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        # building first layer
+        self.features = MobileNetV2Backbone(input_size=input_size,
+                                            width_mult=width_mult)
 
         # building classifier
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(self.last_channel, n_class),
+            nn.Linear(self.features.last_channel, num_classes),
         )
 
         self._initialize_weights()
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(-1, self.last_channel)
+        x = x.view(-1, self.features.last_channel)
         x = self.classifier(x)
         return x
 
