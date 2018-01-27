9777644a5d8da8a9ffc09033a326e6b31c361457
ahirner <a.hirner@gmail.com>
2018-01-27 15:28:09 +0100

    Fix unused local var in Linear init

--- MobileNetV2.py
+++ MobileNetV2.py
@@ -109,7 +109,6 @@ class MobileNetV2(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
